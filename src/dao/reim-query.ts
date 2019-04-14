5ac77bbc3d42095428eb2232a954b1b21197b01b
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-14 18:46:41 -0400

    Reimbursement status is done now

--- src/dao/reim-query.ts
+++ src/dao/reim-query.ts
@@ -4,10 +4,10 @@ const PQ = require('pg-promise').ParameterizedQuery;
 
 export async function findingStatusId(statusId: number) {
     if (statusId  > -1 && statusId < 3) {
-    const findStatus = new PQ('SELECT * from ers_reim_status INNER JOIN ers_reim USING (status_id) WHERE status_id = $1;', [statusId]);
-        return db.one(findStatus)
+    const findStatus = new PQ('SELECT * from ers_reim_status INNER JOIN ers_reim USING (status_id) WHERE status_id = $1 ORDER BY date_submitted;', [statusId]);
+return await db.one(findStatus)
         .then (data => {
-            console.log(data);
+            return data;
         }).catch (error => {
             console.log('ERROR:', error);
         });
--- src/routers/reimbursement-router.ts
+++ src/routers/reimbursement-router.ts
@@ -6,12 +6,14 @@ import { findingStatusId } from '../dao/reim-query';
 export const reimbursementRouter = express.Router();
 // console.log(reimbursements);//PLACEHOLDER TO TEST
 
-reimbursementRouter.get('/status/:statusid', [authMiddleware(['Admin', 'Finance Manager']), (req, res) => {  // Finance-manager has access to this only (+ admin).
+reimbursementRouter.get('/status/:statusid', [authMiddleware(['Admin', 'Finance Manager']), async (req, res) => {  // Finance-manager has access to this only (+ admin).
     const statusid: number = +req.params.statusid;
     console.log(statusid);
-    findingStatusId(statusid);
+    const data = await findingStatusId(statusid);
+    console.log(data);
     if (findingStatusId) {
         console.log('Query results has been found');
+        res.json(data);
     } else {
         console.log('Status has not been found');
         res.sendStatus(404);
